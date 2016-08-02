371634a1177879cb1f985fab459641e26120535e
FerranRJ <ferran.rey@e-campus.uab.cat>
2016-08-02 19:07:31 +0200

    Intento2
    Código que debería mostrar una imagen por pantalla

--- intent/main.cpp
+++ intent/main.cpp
@@ -1,12 +1,31 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <opencv2/core.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    String imageName( "/home/ferran/Escriptori/intent.jpg"); // by default
 
 
-    return a.exec();
+    Mat image;
+    image = imread( imageName, IMREAD_COLOR ); // Read the file
+    if( image.empty() )                      // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
+    cv::namedWindow("Display",WINDOW_AUTOSIZE);
+    cv::imshow("Display",image);
+    cv::waitKey(0);
+    return 0;
 }
