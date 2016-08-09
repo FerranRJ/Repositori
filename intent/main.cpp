5273cf137d35b08fa5e1990c2d1dacaea7718f4e
FerranRJ <ferran.rey@e-campus.uab.cat>
2016-08-09 15:50:24 +0200

    Intento de detectar movimiento

--- intent/main.cpp
+++ intent/main.cpp
@@ -1,31 +1,60 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <opencv2/core.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include <QLabel>
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
-#include <iostream>
-#include <string>
+#include <opencv2/core.hpp>
 
 using namespace cv;
-using namespace std;
-
 
 int main(int argc, char *argv[])
 {
-    String imageName( "/home/ferran/Escriptori/intent.jpg"); // by default
 
-
-    Mat image;
-    image = imread( imageName, IMREAD_COLOR ); // Read the file
-    if( image.empty() )                      // Check for invalid input
+    QApplication a(argc, argv);
+    QImage myImage;
+    myImage.load("/home/ferran/Escriptori/lab1/highway/input/in000486.jpg");
+    QRgb color;
+    QSize sizeImage = myImage.size();
+    int width = sizeImage.width(), height = sizeImage.height();
+    for(int i=0;i<width;i++)
     {
-        cout <<  "Could not open or find the image" << std::endl ;
-        return -1;
+        for(int j=0;j<height;j++){
+            color = myImage.pixel(i, j);
+            int gray = (qRed(color) + qGreen(color) + qBlue(color))/3;
+            myImage.setPixel(i, j, qRgb(gray, gray, gray));
+        }
+
+    }
+    QImage myImage2;
+    myImage2.load("/home/ferran/Escriptori/lab1/highway/input/in000159.jpg");
+    QRgb color2;
+    QSize sizeImage2 = myImage2.size();
+    int width2 = sizeImage2.width(), height2 = sizeImage2.height();
+    for(int i=0;i<width2;i++)
+    {
+        for(int j=0;j<height2;j++){
+            color2 = myImage2.pixel(i, j);
+            int gray = (qRed(color) + qGreen(color) + qBlue(color))/3;
+            myImage2.setPixel(i, j, qRgb(gray, gray, gray));
+        }
+
     }
 
-    cv::namedWindow("Display",WINDOW_AUTOSIZE);
-    cv::imshow("Display",image);
-    cv::waitKey(0);
-    return 0;
+    for(int i=0;i<width2;i++)
+    {
+        for(int j=0;j<height2;j++){
+            color = myImage.pixel(i, j);
+            color2 = myImage2.pixel(i, j);
+            int gray = color2-color;
+            myImage2.setPixel(i, j, qRgb(gray, gray, gray));
+        }
+
+    }
+
+
+    QLabel myLabel;
+    myLabel.setPixmap(QPixmap::fromImage(myImage2));
+    myLabel.show();
+
+   return a.exec();
 }
